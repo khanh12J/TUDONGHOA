223e6057aec683e281e4c48bf56106824efdcd64
khanh12J <nguyenquangkhanh9a7@gmail.com>
2025-03-05 10:19:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function login()
 
 let u username.value
 let p pw.value
-if (u="hieu" && p=="123")
+if (u="wow" && p=="wowbeo")
 {
 window.open("home.html")
 document.getElementById(compiler).style.display="block"
